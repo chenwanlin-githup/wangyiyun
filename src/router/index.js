b95e0448616ae65eea4b50069d3c5c151803ccfd
chenwanlin-githup <2450673382@qq.com>
2020-01-09 09:15:32 +0800

    账号页面和登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,47 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Index from '../views/index.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "../views/index.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'index',
+    path: "/",
+    name: "index",
     component: Index,
     children: [
       {
-        path: '/find',
-        name: 'find',
-        component: () => import('../views/child/findMusic.vue'),
+        path: "/find",
+        name: "find",
+        component: () => import("../views/child/findMusic.vue")
       },
       {
-        path: '/me',
-        name: 'me',
-        component: () => import('../views/child/meMusic.vue'),
+        path: "/me",
+        name: "me",
+        component: () => import("../views/child/meMusic.vue")
       },
       {
-        path: '/friend',
-        name: 'friend',
-        component: () => import('../views/child/friend.vue'),
+        path: "/friend",
+        name: "friend",
+        component: () => import("../views/child/friend.vue")
       },
       {
-        path: '/account',
-        name: 'account',
-        component: () => import('../views/child/account.vue'),
-      },
-    ],
+        path: "/account",
+        name: "account",
+        component: () => import("../views/child/account.vue")
+      }
+    ]
   },
-  //  {
-  //     path: '/about',
-  //     name: 'about',
-  //     // route level code-splitting
-  //     // this generates a separate chunk (about.[hash].js) for this route
-  //     // which is lazy-loaded when the route is visited.
-  //     component: () =>
-  //       import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  //   },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/login.vue")
+  }
 ];
 
 const router = new VueRouter({
-  routes, //配置路由规则的
-  linkActiveClass: 'vant-active', //覆盖默认路由高亮的类
+  routes //配置路由规则的
+  //linkActiveClass: 'vant-active', //覆盖默认路由高亮的类
 });
 
 export default router;
