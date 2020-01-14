873a4f9aee4e541899af4635c423032c815295a5
chenwanlin-githup <2450673382@qq.com>
2020-01-14 14:01:00 +0800

    音乐播放

--- src/views/child/meMusic.vue
+++ src/views/child/meMusic.vue
@@ -1,3 +1,16 @@
-<template>
-  <div>我的音乐</div>
+
+<template >
+  <!-- {"shhs":1,"shhs":2,"shhs":3,"shhs":4} -->
+  <div>
+    <p v-for="(item,id,num) in data" :key="num">{{item[1]}}</p>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      data: { shhsa: 11, shhss: 22, shhsd: 33, shhsf: 444 }
+    };
+  }
+};
+</script>
